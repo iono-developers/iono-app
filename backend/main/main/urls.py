f335f4b440eed4320e2b9659be217877505bea7f
isbre <BresciaDavide@outlook.it>
2024-01-23 14:21:11 +0100

    improved api endpoints

--- backend/main/main/urls.py
+++ backend/main/main/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('events.urls')),
+    path('events/', include('events.urls')),
     path('auth/', include('authentication.urls')),
     path('users/', include('users.urls')),
 ]
