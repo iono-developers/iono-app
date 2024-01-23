56817e5a908d84847f37eda77b37e2623320b5ea
isbre <BresciaDavide@outlook.it>
2024-01-23 14:21:37 +0100

    improved api endpoints

--- backend/main/events/urls.py
+++ backend/main/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import EventListCreateView, InviteListCreateView, LoserListCreateView
 
 urlpatterns = [
-    path('/', EventListCreateView.as_view(), name='event-list-create'),
+    path('', EventListCreateView.as_view(), name='event-list-create'),
     path('invites/', InviteListCreateView.as_view(), name='invite-list-create'),
     path('loser/', LoserListCreateView.as_view(), name='loser-list-create'),
 ]
