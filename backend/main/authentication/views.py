6e16c93fee4fb2e2febc2673e2f91f1f2ca67a1f
isbre <BresciaDavide@outlook.it>
2024-01-24 22:24:45 +0100

    improved the authentication jwt informations

--- backend/main/authentication/views.py
+++ backend/main/authentication/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .serializers import TokenSerializer
 
 
