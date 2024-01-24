b3055e6a30a0807610447a8c1e2e93c6b4e9b356
isbre <BresciaDavide@outlook.it>
2024-01-24 22:24:08 +0100

    improved the authentication jwt informations

--- backend/main/authentication/serializers.py
+++ backend/main/authentication/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from .models import User
+
+
+# I could have easily used the default TokenObtainPairSerializer, 
+# but I believe that preserving the username is crucial. 
+# Therefore, I extended the class and added this feature.
+class TokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
--- backend/main/authentication/views.py
+++ backend/main/authentication/views.py
@@ -3,7 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import TokenSerializer
 
+
+class TokenView(TokenObtainPairView):
+    serializer_class = TokenSerializer
 
 class LogoutView(APIView):
      permission_classes = (IsAuthenticated,)
