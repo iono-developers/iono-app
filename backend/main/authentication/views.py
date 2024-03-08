3b9e4c30d46bd3031246103f9ce00b6f20e69965
isbre <BresciaDavide@outlook.it>
2024-03-08 17:39:01 +0100

    improved imports display

--- backend/main/authentication/views.py
+++ backend/main/authentication/views.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
+
+from .serializers import TokenSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-from .serializers import TokenSerializer
 
 
 class TokenView(TokenObtainPairView):
